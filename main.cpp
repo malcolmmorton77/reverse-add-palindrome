1904d0ffca8e82fd0486fd0ef522a65f92c411e6
malcolmmorton77 <malcolmmorton77@gmail.com>
2022-03-21 19:44:30 -0500

    wrote up two functions, one that calculates the reverse number and the driver function in main.

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+//reverses the given number and returns it as an integer so that we can add it.
+long long int reverseNum(long long int n){
+    int rev = 0, cpy = n;
+
+    string revstr;
+    while(n > 0){
+        rev = n%10;
+        revstr += to_string(rev);
+        n /= 10;
+    }
+    if(cpy <= 1794102595)
+        rev = stoi(revstr);
+    else{
+        cerr << "No palindrome exists";
+        exit(1);
+    }
+    return rev;
+}
+
+int main(){
+    long long int count = 0, number = 196; // declare count for number of iterations required to get a palindrome
+
+    while(number != reverseNum(number)){
+        int reverse = reverseNum(number);
+        number = number + reverse;
+        count++;
+    }
+
+    cout << count << " " << number << endl;
+
+    return 0;
+}
